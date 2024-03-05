a6287351232345fe5f94819e1ea45aef53a94e62
Maximka <139463051+InstiSher@users.noreply.github.com>
2024-03-05 11:02:50 -0800

    Add files via upload

---
+++ 3nd project (parser-converter)/main.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def main():
+    pass
+
+    # data =  Можем записать дату и передавать в url
+    url = 'https://cbr.ru/currency_base/daily/'
+
+    link = requests.get(url)
+    result = link.content
+
+    soup = BeautifulSoup(result, features='lxml')
+    money = soup.find_all('td')
+
+    money_data = []  # Конечное
+    data = []  # Вся инфа
+
+    for item in money:
+        data.append(item.text)
+
+    for count in range(0, len(data), 5):
+        money_dict = {
+            'Digits. code': data[count],
+            'Letters. code': data[count+1],
+            'Units': data[count+2],
+            'Currency': data[count+3],
+            'Course': data[count+4]
+        }
+        money_data.append(money_dict)
+
+    with open('money.json', 'w') as json_file:
+        json.dump(money_data, json_file, indent=4)
+
+if __name__ == '__main__':
+    main()
+    with open('money.json') as f:
+        data = json.load(f)
+        print(data)
+
