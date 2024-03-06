f85603ced47288fa148de76c181747635faea928
Maximka <139463051+InstiSher@users.noreply.github.com>
2024-03-06 08:44:23 -0800

    Add files via upload

---
+++ 3nd project (parser-converter)/Converter.py
@@ -0,0 +1,15 @@
+import json
+
+
+with open('money.json') as f:
+    data = json.load(f)
+
+
+money_start = input('Введите интересующую валюту: ')
+for i in data:
+    if i['Letters. code'] == money_start:
+        money = int(input(f'Введите деньги в {i['Currency']} '))
+        multiply = round(money/int(i['Units']), 3)
+        money_end = multiply * float(i['Course'])
+        print(f'{money} {i['Currency']} в рубля будет {money_end}')
+
