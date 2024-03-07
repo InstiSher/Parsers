2143111d532d9131bb590df19fecd821436cc2ff
Maximka <139463051+InstiSher@users.noreply.github.com>
2024-03-07 11:58:28 -0800

    Add files via upload

--- 3nd project (parser-converter)/Converter.py
+++ 3nd project (parser-converter)/Converter.py
@@ -5,12 +5,31 @@ with open('money.json') as f:
     data = json.load(f)
 
 
-money_start = input('Введите интересующую валюту: ')
-for i in data:
-    if i['Letters. code'] == money_start:
-        money = int(input(f'Введите деньги в {i['Currency']} '))
-        multiply = round(money/int(i['Units']), 3)
-        money_end = multiply * float(i['Course'])
-        print(f'{money} {i['Currency']} в рублях будет {money_end}')
-        break
+def ConverterVsego():
+    kakoi_perevod = input('Введите какой перевод хотите: PUB/INO ')
 
+    if kakoi_perevod == 'PUB':
+        money_start = input('Введите интересующую валюту: ')
+        for i in data:
+            if i['Letters. code'] == money_start:
+                money = float(input(f'Вы выбрали {i['Currency']}\nВведите деньги в Рублях: '))
+                multiply = round(money / round(float(i['Course'])), 3)
+                money_end = multiply * float(i['Units'])
+                print(f'{money} Рублей в {i['Currency']} будет {money_end}')
+                break
+
+    elif kakoi_perevod == 'INO':
+        money_start = input('Введите интересующую валюту: ')
+        for i in data:
+            if i['Letters. code'] == money_start:
+                money = int(input(f'Введите деньги в {i['Currency']} '))
+                multiply = round(money/int(i['Units']), 3)
+                money_end = multiply * float(i['Course'])
+                print(f'{money} {i['Currency']} в рублях будет {money_end}')
+                break
+
+    else:
+        print('Неверный ввод, повторите попытку:')
+        ConverterVsego()
+
+ConverterVsego()
