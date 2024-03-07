20e41aac297726d622190c6bda30251799b33918
Maximka <139463051+InstiSher@users.noreply.github.com>
2024-03-07 12:09:38 -0800

    Add files via upload

--- 3nd project (parser-converter)/Converter.py
+++ 3nd project (parser-converter)/Converter.py
@@ -12,21 +12,37 @@ def ConverterVsego():
         money_start = input('Введите интересующую валюту: ')
         for i in data:
             if i['Letters. code'] == money_start:
-                money = float(input(f'Вы выбрали {i['Currency']}\nВведите деньги в Рублях: '))
-                multiply = round(money / round(float(i['Course'])), 3)
-                money_end = multiply * float(i['Units'])
-                print(f'{money} Рублей в {i['Currency']} будет {money_end}')
-                break
+                try:
+                    money = float(input(f'Вы выбрали {i['Currency']}\nВведите деньги в Рублях: '))
+                    multiply = round(money / round(float(i['Course']), 3), 3)
+                    money_end = multiply * float(i['Units'])
+                    print(f'{money} Рублей в {i['Currency']} будет {money_end}')
+                    print('data')
+                    break
+                except Exception as e:
+                    print(e)
+                    print('Неправильный ввод попробуйте снова')
+                    break
+        else:
+            print('Извините, такой валюты нет')
 
     elif kakoi_perevod == 'INO':
         money_start = input('Введите интересующую валюту: ')
         for i in data:
             if i['Letters. code'] == money_start:
-                money = int(input(f'Введите деньги в {i['Currency']} '))
-                multiply = round(money/int(i['Units']), 3)
-                money_end = multiply * float(i['Course'])
-                print(f'{money} {i['Currency']} в рублях будет {money_end}')
-                break
+                try:
+                    money = int(input(f'Введите деньги в {i['Currency']} '))
+                    multiply = round(money/int(i['Units']), 3)
+                    money_end = multiply * float(i['Course'])
+                    print(f'{money} {i['Currency']} в рублях будет {money_end}')
+                    print('data')
+                    break
+                except Exception as e:
+                    print(e)
+                    print('Неправильный ввод попробуйте снова')
+                    break
+        else:
+            print('Извините, такой валюты нет')
 
     else:
         print('Неверный ввод, повторите попытку:')
